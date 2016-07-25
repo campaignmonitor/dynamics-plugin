ef8988da8b19e3fc3023e2773b81967b47b24d35
Hayden Thomas <hthomas@sonomapartners.com>
2016-07-25 11:17:08 -0500

    SendMessagePlugin - Layout the framework and implementation plan for the message that will send the sync data to campaign monitor.

---
+++ Campmon.Dynamics.Plugins/Logic/SendMessageLogic.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk;
+
+namespace Campmon.Dynamics.Plugins.Logic
+{
+    class SendMessageLogic
+    {
+        IOrganizationService _orgService;
+        ITracingService _tracer;
+
+        public SendMessageLogic(IOrganizationService orgService, ITracingService tracer)
+        {
+            _orgService = orgService;
+            _tracer = tracer;
+        }
+
+        public void SendMessage(Entity target)
+        {
+            ConfigurationService configService = new ConfigurationService(_orgService);
+            CampaignMonitorConfiguration campaignMonitorConfig = configService.LoadConfig();
+
+            if (!campaignMonitorConfig.SyncDuplicateEmails)
+            {
+                // check if email already exists for any contact already synced 
+                // if yes : set campmon_error on message to duplicate email 
+            }
+
+            ConvertSchemaNamesToCampaignMonitor();
+
+            try
+            {
+                SendSubscriberToList(campaignMonitorConfig.ListId);
+            }
+            catch (Exception ex)
+            {
+                // if error on sending subscriber, set campmon_error on msg to the error
+                return;
+            }
+
+            // deactivate msg if successful create/update
+        }
+
+        internal void ConvertSchemaNamesToCampaignMonitor()
+        {
+            // convert each field to Campaign Monitor custom 
+            // field names by using the display name for the field
+        }
+
+        internal void SendSubscriberToList(string listId)
+        {
+            // send subscriber to campaign monitor list using CM API            
+        }
+    }
+}
---
+++ Campmon.Dynamics.Plugins/SendMessagePlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SonomaPartners.Crm.Toolkit.Plugins;
+using SonomaPartners.Crm.Toolkit;
+using Campmon.Dynamics.Plugins.Logic;
+using Microsoft.Xrm.Sdk;
+
+namespace Campmon.Dynamics.Plugins
+{
+    class SendMessagePlugin : PluginBase
+    {
+        public override void OnExecute(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetPluginExecutionContext();
+            var orgService = serviceProvider.CreateOrganizationServiceAsCurrentUser();
+            var tracer = serviceProvider.GetTracingService();
+
+            if (!context.InputParameters.Contains("Target"))
+            {
+                return;
+            }
+
+            Entity target = (Entity)context.InputParameters["Target"];
+
+            SendMessageLogic logic = new SendMessageLogic(orgService, tracer);
+            logic.SendMessage(target);
+        }
+    }
+}
