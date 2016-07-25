f2bf48ded8d303fc77ae50b4686d17a993761afd
Hayden Thomas <hthomas@sonomapartners.com>
2016-07-25 09:46:06 -0500

    ContactSyncPlugin - A little bit more cleanup and clearer documentation with comments

--- Campmon.Dynamics.Plugins/Logic/ContactSyncLogic.cs
+++ Campmon.Dynamics.Plugins/Logic/ContactSyncLogic.cs
@@ -21,15 +21,24 @@ namespace Campmon.Dynamics.Plugins.Logic
 
         internal void SyncContact(Entity target, Entity postImage, bool isUpdate)
         {
-            // 3. Check that the email field in campmon_emailaddress has data in the contact post image. If it is empty, exit the plugin.
-            if (target == null || postImage == null || postImage["campmon_emailaddress"] == null)
+            if (target == null || postImage == null)
             {
                 return;
             }
 
-            // 1. Retrieve the Campaign Monitor Configuration record. If it does not exist or is missing access token or client id, exit the plugin.
+            // Check that the email field in campmon_emailaddress has data in the contact post image. If it is empty, exit the plugin.
+            //?
+
+            /*
+            if (!postImage.Contains("campmon_emailaddress") || postImage["campmon_emailaddress"] == null)
+            {
+                return; 
+            } 
+            */
+
+            // Retrieve the Campaign Monitor Configuration record. If it does not exist or is missing access token or client id, exit the plugin.
             ConfigurationService configService = new ConfigurationService(_orgService);
-            CampaignMonitorConfiguration campaignMonitorConfig = configService.LoadConfig();            
+            CampaignMonitorConfiguration campaignMonitorConfig = configService.LoadConfig();
             if (campaignMonitorConfig == null ||
                     campaignMonitorConfig.AccessToken == null || campaignMonitorConfig.ClientId == null)
             {
@@ -37,44 +46,30 @@ namespace Campmon.Dynamics.Plugins.Logic
                 return;
             }
 
-            // 2. Retrieve the view specified in the campmon_syncviewid field of the configuration record.
+            // Retrieve the view specified in the campmon_syncviewid field of the configuration record.
             var filterView = RetrieveSyncFilter(campaignMonitorConfig.SyncViewId);
             var filterFetchXml = filterView["fetchxml"].ToString();
 
-            // 4.Modify the sync view fetch query to include a filter condition for the current contact id.Execute the modified query and check if the contact is returned.If it is, exit the plugin.
+            // Modify the sync view fetch query to include a filter condition for the current contact id.Execute the modified query and check if the contact is returned.If it is, exit the plugin.
             if (!TestContactFitsFilter(filterFetchXml, (Guid)target["contactid"]))
             {
                 _tracer.Trace("Contact does not fit the filter.");
                 return;
-            }
-
-            // 5. If this is an update operation, check that the plugin target has modified attributes that are included in the campmon_syncfields data. If there are not any sync fields in the target, exit the plugin.
-
-            // this is ugly; it should work to just test the return string of serialize method and if == {} then no fields are updated and don't sync?
-/*          if (isUpdate)
-            {
-                var st = new List<String>();
-                // add fields to a collection
-                foreach (var field in campaignMonitorConfig.SyncFields)
-                {
-                    if (target.Attributes.Contains(field))
-                    {
-                        st.Add(field);
-                    }
-                }
-
-                if (st.Count <= 0) return;
-            }*/
+            }           
 
             /*
-                6. Create a campmon_message record with the following data:
+                Create a campmon_message record with the following data:
                     • campmon_sdkmessage = Plugin message (create or update)
                     • campmon_data = JSON serialized sync data
             */
             var syncMessage = new Entity("campmon_message");
             var syncData = SerializeDataToSync(target, campaignMonitorConfig);
 
-            if (syncData == "{}") { return; }
+            // If this is an update operation, check that the plugin target has modified attributes that are included in the campmon_syncfields data. If there are not any sync fields in the target, exit the plugin.
+            if (syncData == "{}")
+            {
+                return;
+            }
 
             syncMessage["campmon_sdkmessage"] = isUpdate ? "update" : "create";
             syncMessage["campmon_data"] = syncData;
@@ -108,13 +103,9 @@ namespace Campmon.Dynamics.Plugins.Logic
 
         internal string SerializeDataToSync(Entity target, CampaignMonitorConfiguration config)
         {
-            /*
-                   7. To serialize the sync data, create a single object with each field schema name as the property with its associated value. Use the following rules to convert data:
-                       • To transform Lookup and Option Set fields, use the text label and send as text
-                       • To transform date fields, send as date
-                       • To transform numeric fields, send as a number
-                       • For any other fields, send as text
-               */
+            //  To serialize the sync data, create a single object with each 
+            //  field schema name as the property with its associated value.
+
             var syncData = "{";
             foreach (var field in config.SyncFields)
             {
@@ -122,9 +113,10 @@ namespace Campmon.Dynamics.Plugins.Logic
                 {
                     continue;
                 }
-
+                
                 if (target.Attributes[field].GetType() == typeof(EntityReference))
                 {
+                    // To transform Lookup and Option Set fields, use the text label and send as text
                     var refr = (EntityReference)target.Attributes[field];
                     syncData += string.Format("{0}:\"{1}\",", field, refr.Name);
                 }
@@ -135,16 +127,18 @@ namespace Campmon.Dynamics.Plugins.Logic
                 }
                 else if (target.Attributes[field].GetType() == typeof(DateTime))
                 {
+                    // To transform date fields, send as date
                     var date = (DateTime)target.Attributes[field];
-                    syncData += string.Format("{0}:{1},", field, date.ToLongDateString()); //?
+                    syncData += string.Format("{0}:{1},", field, date.ToLongDateString()); //? or \Date(#)\?
                 }
                 else if (IsNumeric(target.Attributes[field]))
                 {
+                    // To transform numeric fields, send as a number
                     syncData += string.Format("{0}:{1},", field, target.Attributes[field].ToString());
                 }
                 else
                 {
-                    // quotes around {1} to signify string val
+                    // For any other fields, send as text
                     syncData += string.Format("{0}:\"{1}\",", field, target.Attributes[field].ToString());
                 }
             }
@@ -161,5 +155,5 @@ namespace Campmon.Dynamics.Plugins.Logic
             bool isNum = Double.TryParse(Convert.ToString(Expression), System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
             return isNum;
         }
-    }    
+    }
 }
