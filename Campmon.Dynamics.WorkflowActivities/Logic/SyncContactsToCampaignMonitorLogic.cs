2d1cf6fc78f64697447dd7ac048bad3b3b436087
Hayden Thomas <hthomas@sonomapartners.com>
2016-07-26 13:23:19 -0500

    SyncContacts WorkFlow - Set up basic skeleton for workflow activity.

---
+++ Campmon.Dynamics.WorkflowActivities/Logic/SyncContactsToCampaignMonitorLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk;
+
+namespace Campmon.Dynamics.WorkflowActivities.Logic
+{
+    class SyncContactsToCampaignMonitorLogic
+    {
+        IOrganizationService _orgService;
+        ITracingService _tracer;
+
+        public SyncContactsToCampaignMonitorLogic(IOrganizationService orgService, ITracingService tracer)
+        {
+            _orgService = orgService;
+            _tracer = tracer;
+        }
+
+        public bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
