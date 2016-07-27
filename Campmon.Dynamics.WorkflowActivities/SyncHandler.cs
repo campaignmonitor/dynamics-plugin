3a4d60d692292281a3623037b53f885037a98ecb
Hayden Thomas <hthomas@sonomapartners.com>
2016-07-27 18:08:51 -0500

    SyncContactsToCampaignMonitorWorkflow - Implement logic into SyncHandler to sync batches of 1000 contacts using the Import API

--- Campmon.Dynamics.WorkflowActivities/SyncHandler.cs
+++ Campmon.Dynamics.WorkflowActivities/SyncHandler.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 using Campmon.Dynamics;
+using Campmon.Dynamics.Logic;
 using Newtonsoft.Json;
+using createsend_dotnet;
+using System.Collections.Generic;
 
 namespace Campmon.Dynamics.WorkflowActivities
 {
@@ -25,7 +26,7 @@ namespace Campmon.Dynamics.WorkflowActivities
 
             var configService = new ConfigurationService(orgService);
             trace.Trace("Loading configuration.");
-            config = configService.LoadConfig();
+            config = configService.VerifyAndLoadConfig();
         }
 
         public bool Run()
@@ -36,14 +37,82 @@ namespace Campmon.Dynamics.WorkflowActivities
             // get view and modify.
             // if syncData.UpdatedFields contains data, only sync those fields.
 
+            var primaryEmail = SharedLogic.GetPrimaryEmailField(config.SubscriberEmail);
+
+            QueryExpression viewFilter = SharedLogic.GetConfigFilterQuery(orgService, config.SyncViewId);
+            if (syncData.UpdatedFields.Length > 0)
+            {
+                viewFilter.ColumnSet.Columns.Clear();
+                syncData.UpdatedFields.ToList().ForEach(x => viewFilter.ColumnSet.Columns.Add(x));
+            }
+
+            // we need the correct email for the subscription, and potentially the fullname(?)
+            // need some sort of name field otherwise it will be blank on CM
+            if (!viewFilter.ColumnSet.Columns.Contains("fullname"))
+            {
+                viewFilter.ColumnSet.Columns.Add("fullname");
+            }
+
+            if (!viewFilter.ColumnSet.Columns.Contains(primaryEmail))
+            {
+                viewFilter.ColumnSet.Columns.Add(primaryEmail);
+            }
+
+            if (syncData.PageNumber >= 0)
+            {
+                viewFilter.PageInfo.PageNumber = syncData.PageNumber;
+            }
+
+            if (!string.IsNullOrWhiteSpace(syncData.PagingCookie))
+            {
+                viewFilter.PageInfo.PagingCookie = syncData.PagingCookie;
+            }
+
+            viewFilter.AddOrder("modifiedon", OrderType.Ascending);
+            viewFilter.TopCount = 1000;
+
+            var auth = SharedLogic.GetAuthentication(config);
+            var sub = new Subscriber(auth, config.ListId);
+            List<SubscriberDetail> subscribers = new List<SubscriberDetail>();
+
             do
             {
                 // sync batch of 1000 contacts to CM list as subscribers
+                EntityCollection contacts = orgService.RetrieveMultiple(viewFilter);
+                foreach (Entity contact in contacts.Entities)
+                {
+                    var email = contact.Attributes[primaryEmail].ToString();
+                    var name = contact.Attributes["fullname"].ToString();
+                    contact.Attributes.Remove(primaryEmail);
+                    contact.Attributes.Remove("fullname");
+
+
+                    SubscriberDetail sd = new SubscriberDetail();
+                    
+                    var fields = SharedLogic.ContactAttributesToSubscriberFields(orgService, contact, contact.Attributes.Keys);
+                    subscribers.Add(new SubscriberDetail(email, name, fields));                    
+                }
+
+                BulkImportResults importResults = sub.Import(subscribers, false);
+                //if (importResults.FailureDetails.Count > 0)
+                //{
+                //    // some failures occurred.
+                //    foreach (var failure in importResults.FailureDetails)
+                //    {
+                //        // do something here?
+                //        //failure.Code
+                //        //failure.EmailAddress
+                //        //failure.Message
+                //    }
+                //}
+                
+                viewFilter.PageInfo.PagingCookie = syncData.PagingCookie = contacts.PagingCookie;
+                viewFilter.PageInfo.PageNumber = syncData.PageNumber = syncData.PageNumber + 1; // ?? contacts.PageNumber isn't a thing
             }
             while (timer.ElapsedMilliseconds >= 90000);
 
             return true;
             throw new NotImplementedException();
-        }
+        }        
     }
 }
