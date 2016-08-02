02c34b389822cd0d5c1b76be8cf625d6b920bf7d
Hayden Thomas <hthomas@sonomapartners.com>
2016-08-02 10:34:50 -0500

    Selector for primary email and lay the framework for loading the config into the form.

--- Campmon.Dynamics.WebResources/Scripts/configurationPage.js
+++ Campmon.Dynamics.WebResources/Scripts/configurationPage.js
@@ -1,56 +1,84 @@
 ﻿/// <reference path="Libraries/webApiRest.js" />
 /// <reference path="Libraries/knockout340.js" />
 
+(function () {
+    "use strict";
 
-function CampMonViewModel() {
-    var self = this;
+    var CampaignMonitorConfig = (function () {
+        
+        // todo: load config, then the observables may be set up based on CampaignMonitorConfig.<attr>        
+        
+        function saveConfig() {
+            // todo: Implement
+        }
 
-    self.clients = ko.observableArray();
-    self.clientLists = ko.observableArray();
-
-    self.selectedClient = ko.observable();
-    self.selectedList = ko.observable();
-    self.hasConnectionError = ko.observable(false);
-    self.isClientSelected = ko.observable(false);
-}
-var cm = new CampMonViewModel();
-ko.applyBindings(cm);
-
-
-
-var CMPlugin = (function () {
-    const _pluginAction = 'campmon_ExecuteOperationAction';
-    var pluginInput = function (operation, data) {
         return {
-            OperationName: operation,
-            InputData: data
+            AccessToken: "",
+            BulkSyncData: "",
+            BulkSyncInProgress: false,
+            ClientId: "",
+            ClientName: "",
+            ListId: "",
+            ListName: "",
+            SetUpError: "",
+            SyncDuplicateEmails: false,
+            SyncFields: [],
+            SyncViewId: "",
+            SyncViewName: "",
+            SubscriberEmail: "778230000",
+            save: saveConfig
         };
+    }());
+    
+    function CampMonViewModel(config) {
+        var self = this;
+
+        self.clients = ko.observableArray();
+        self.clientLists = ko.observableArray();
+
+        self.selectedClient = ko.observable();
+        self.selectedList = ko.observable();
+        self.hasConnectionError = ko.observable(false);
+        self.isClientSelected = ko.observable(false);
+
+        self.selectedPrimaryEmail = ko.observable(config.SubscriberEmail);
     }
-    var executeAction = function (action, data) {
-        return webAPI.REST.executeUnboundAction(_pluginAction, pluginInput(action, data), null);
-    }
-    return {
-        executeAction: executeAction
-    }
-})();
 
+    var cm = new CampMonViewModel(CampaignMonitorConfig);
+    ko.applyBindings(cm);
 
-CMPlugin.executeAction('getclients', null).
- then(function (result) {
-     cm.clients(JSON.parse(result.body.OutputData));
-     cm.isClientSelected(true);
- }, function (error) {
-     cm.hasConnectionError = true;
-     console.log(JSON.parse(error.response.text));
- });
+    var CMPlugin = (function () {
+        const _pluginAction = 'campmon_ExecuteOperationAction';
+        var pluginInput = function (operation, data) {
+            return {
+                OperationName: operation,
+                InputData: data
+            };
+        }
+        var executeAction = function (action, data) {
+            return webAPI.REST.executeUnboundAction(_pluginAction, pluginInput(action, data), null);
+        }
+        return {
+            executeAction: executeAction
+        }
+    })();
 
+    CMPlugin.executeAction('getclients', null).
+     then(function (result) {
+         cm.clients(JSON.parse(result.body.OutputData));
+         cm.isClientSelected(true);
+     }, function (error) {
+         cm.hasConnectionError = true;
+         console.log(JSON.parse(error.response.text));
+     });
 
-cm.selectedClient.subscribe(function (selectedClient) {
-    CMPlugin.executeAction('getclientlist', selectedClient).
-    then(function (result) {
-        //TODO: If no client lists default to Sync to New List Option
-        cm.clientLists(JSON.parse(result.body.OutputData));
-    }, function (error) {
-        alert("Error retrieving lists for selected client.");
+    cm.selectedClient.subscribe(function (selectedClient) {
+        CMPlugin.executeAction('getclientlist', selectedClient).
+        then(function (result) {
+            //TODO: If no client lists default to Sync to New List Option
+            cm.clientLists(JSON.parse(result.body.OutputData));
+        }, function (error) {
+            alert("Error retrieving lists for selected client.");
+        });
     });
-});
+}());
