8561f786af102b706cf74cce379add404b6efb49
Hayden Thomas <hthomas@sonomapartners.com>
2016-08-03 12:22:42 -0500

    Pull/preselect checkboxes for fields. Pull/preselect the view for the config. Preselect the sync duplicates field. Hide subscriber email fields if they are not a selected field. Todo: Add additional logic to hiding the subscriber email fields s.t. the user cannot unselect all 3 fields.

--- Campmon.Dynamics.WebResources/Scripts/configurationPage.js
+++ Campmon.Dynamics.WebResources/Scripts/configurationPage.js
@@ -11,7 +11,9 @@
 
             self.clients = ko.observableArray();
             self.clientLists = ko.observableArray();
+            self.views = ko.observableArray();
 
+            self.selectedView = ko.observable();
             self.selectedClient = ko.observable();
             self.selectedList = ko.observable();
             self.hasConnectionError = ko.observable(false);
@@ -23,10 +25,18 @@
             self.disconnect = ko.observable();
 
             self.fields = ko.observableArray();
+            self.fieldChanged = ko.observable();
 
             self.syncDuplicateEmails = ko.observable();
+
+            self.ObservableField = function (logicalName, displayName, isChecked, isRecommended) {
+                var self = this;
+                self.LogicalName = ko.observable(logicalName);
+                self.DisplayName = ko.observable(displayName);
+                self.IsChecked = ko.observable(isChecked);
+                self.IsRecommended = ko.observable(isRecommended);
+            };
           
-            // clean these up once fields are correctly implemented
             self.email1Selected = ko.observable(true);
             self.email2Selected = ko.observable(true);
             self.email3Selected = ko.observable(true);
@@ -57,16 +67,39 @@
                         alert("Error: " + error);
                     });
             });
+
+            vm.fieldChanged.subscribe(function (field) {
+
+                // todo: add logic to select subscriber email if fields are hidden
+                // e.g. if subscriber email = Email Address 2 and we hide it, then default it to whatever is still shown
+
+                // todo: If user deselects all 3 email fields, give an error stating that at least one must be chosen
+                // and do not uncheck the box they last selected
+                if (field.LogicalName() === "emailaddress1") {
+                    vm.email1Selected(field.IsChecked());
+                } else if (field.LogicalName() === "emailaddress2") {
+                    vm.email2Selected(field.IsChecked());
+                } else if (field.LogicalName() === "emailaddress3") {
+                    vm.email3Selected(field.IsChecked());
+                }
+
+                return true;
+            });
             
             ko.applyBindings(vm);
 
-            Campmon.Plugin.executeAction('loadmetadata', null)
+            Campmon.Plugin.executeAction('loadmetadata', "")
                 .then(function (result) {
-                    var config = JSON.parse(result.body.OutputData);
+                    var config = JSON.parse(result.body.OutputData);                    
+
                     if (config.Error) {
                         alert(config.Error);
+                        return;
                     }
-                    vm.clients(config.Clients);
+
+                    if (config.Clients) {
+                        vm.clients(config.Clients);
+                    }
 
                     if (vm.clients().length == 1) {
                         vm.selectedClient(vm.clients()[0]);
@@ -75,49 +108,39 @@
                     if (config.SubscriberEmail) {
                         vm.selectedPrimaryEmail(config.SubscriberEmail.toString());
                     }
-                    
-                    // ko best practices: use the underlying array as to not repaint every time a field is added
-                    var fieldArr = vm.fields();
-                    fieldArr.push(new ObservableField("email", "Email", true, true));
-                    fieldArr.push(new ObservableField("emailaddress2", "Email Address 2", true, true));
-                    fieldArr.push(new ObservableField("emailaddress3", "Email Address 3", true, true));
 
-                    function ObservableField(logicalName, displayName, isChecked, isRecommended) {
-                        var self = this;
-                        self.LogicalName = ko.observable(logicalName);
-                        self.DisplayName = ko.observable(displayName);
-                        self.IsChecked = ko.observable(isChecked);
-                        self.IsRecommended = ko.observable(isRecommended);
-                    }
+                    addAndSelectView(vm, config.Views);                                       
+                    addFields(vm, config.Fields);                    
 
-                    if (config.Fields) {                        
-                        for (var field in config.Fields) {
-                            fieldArr.push(new ObservableField(field.LogicalName, field.DisplayName, field.IsChecked, field.IsRecommended));
-                        }                        
-                    }
-
-                    vm.fields.valueHasMutated();
-
-                    vm.fields().every(function (f) {
-                        debugger;
-                        if (f.LogicalName() === "email") {
-                            f.IsChecked.subscribe(function (newVal) { vm.email1Selected(newVal); });
-                        } else if (f.LogicalName() === "emailaddress2") {
-                            f.IsChecked.subscribe(function (newVal) { debugger; vm.email2Selected(newVal); });
-                        } else if (f.LogicalName() === "emailaddress3") {
-                            f.IsChecked.subscribe(function (newVal) { vm.email3Selected(newVal); });
-                        }
-                    });
-
-                    if (config.SyncDuplicateEmails) {
-                        vm.syncDuplicateEmails(config.SyncDuplicateEmails.toString());
-                    }
-                    
+                    vm.syncDuplicateEmails(config.SyncDuplicateEmails.toString());
                     vm.isLoading(false);
                 }, function (error) {
-                    vm.hasConnectionError = true;
+                    vm.hasConnectionError(true);
                     console.log(JSON.parse(error.response.text));
                 });
+        }
+
+        function addAndSelectView(vm, views) {
+            var viewsArr = [];
+            var selectedViewId = "";
+
+            views.forEach(function (view) {
+                viewsArr.push(view);
+                if (view.IsSelected) {
+                    selectedView = view.ViewId;
+                }
+            });
+
+            vm.views(viewsArr);
+            vm.selectedView(selectedViewId);
+        }
+
+        function addFields(vm, fields) {            
+            var fieldArr = [];
+            fields.forEach(function (field) {
+                fieldArr.push(new vm.ObservableField(field.LogicalName, field.DisplayName, field.IsChecked, field.IsRecommended));
+            });
+            vm.fields(fieldArr);
         }
 
         return {
