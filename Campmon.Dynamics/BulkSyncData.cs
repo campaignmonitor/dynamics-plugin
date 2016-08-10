a5ae325809ba10be5cbf619dd37f552e6a87f9bd
Ian Moore <imoore@sonomapartners.com>
2016-08-10 15:39:30 -0500

    move bulk sync files:

---
+++ Campmon.Dynamics/BulkSyncData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campmon.Dynamics
+{
+    public class BulkSyncData
+    {
+        public BulkSyncData()
+        {
+
+        }
+
+        public string PagingCookie { get; set; }
+        public int PageNumber { get; set; }
+        public string[] UpdatedFields { get; set; }
+        public List<BulkSyncError> BulkSyncErrors { get; set; }
+    }
+}
