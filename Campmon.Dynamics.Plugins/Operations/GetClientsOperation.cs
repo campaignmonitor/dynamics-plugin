92e67c5660a6794c580cc9568e9d36f09ef9ec8c
Ian Moore <imoore@sonomapartners.com>
2016-08-22 14:27:58 -0500

    hide client dropdown if not agency account

---
+++ Campmon.Dynamics.Plugins/Operations/GetClientsOperation.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using createsend_dotnet;
+namespace Campmon.Dynamics.Plugins.Operations
+{
+    public class GetClientsOperation : IOperation
+    {
+        ConfigurationService _cmService { get; set; }
+        public GetClientsOperation(ConfigurationService cmService)
+        {
+            _cmService = cmService;
+        }
+
+        public string Execute(string serializedData)
+        {
+            var config = _cmService.VerifyAndLoadConfig();
+            var auth = Authenticator.GetAuthentication(config);
+            var clients = new General(auth).Clients();
+            return JsonConvert.SerializeObject(clients);
+        }
+    }
+}
